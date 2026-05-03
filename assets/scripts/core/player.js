89e8a9670844c191c8099a3c2c3b1408a2f1dde8
rohanis0000 <123boigetmoney@gmail.com>
2026-05-03 15:53:41 -0400

    console log removal #2

--- assets/scripts/core/player.js
+++ assets/scripts/core/player.js
@@ -1549,7 +1549,6 @@ if (this.p.isFlying || this.p.isUfo) {
     }
   }
   flipGravity(flipped, _0x11bbde = 0.5) {
-      console.log("flipGravity called: flipped=" + flipped + " current=" + this.p.gravityFlipped);
       if (this.p.gravityFlipped === flipped) {
         return;
       }
